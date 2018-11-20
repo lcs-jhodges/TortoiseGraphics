ecb33d3246c04ec638bf7353d00b76f514c235ab
lcs-jhodges <jhodges@lcs.on.ca>
2018-11-20 15:17:18 -0500

    second commit

--- Playground/Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -33,29 +33,29 @@ canvas.drawing { turtle in
     turtle.right(90)
     turtle.curve(withSides: 150, withSize: 10, drawSides: 38)
     turtle.penUp()
-    turtle.goto(0,0)
+    turtle.goto(-12,0)
     turtle.penDown()
-
-
     
-   for _ in 1...14 {
-        turtle.right(10)
+    
+    
+    for _ in 1...10 {
+        turtle.right(2)
         
         for _ in 1...14 {
-            turtle.right(10)
-            turtle.forward(2.5)
+            turtle.right(40)
+            turtle.forward(20)
         }
         for _ in 1...14 {
-            turtle.right(20)
-            turtle.forward(5)
-    }
-    
-        for _ in 1...14 {
-            turtle.right(18)
-            turtle.forward(6)
+            turtle.right(40)
+            turtle.forward(20)
         }
-    
-    
+        
+        for _ in 1...14 {
+            turtle.right(36)
+            turtle.forward(24)
+        }
+        
+        
     }
     
 }
